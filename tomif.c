7292190989175b6c3fbf7c9ecd74deee6868ae61
Vivek Mogalapalli <vivek94@hotmail.com>
2015-03-24 03:18:34 -0400

    Added mv, add, sub, mvnz and wrote the .mif file

--- tomif.c
+++ tomif.c
@@ -2,8 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define WIDTH 16
+#define DEPTH 128
+
 enum Assembly {
+	mv = 0,
 	mvi = 1,
+	add = 2,
+	sub = 3,
 	ld = 4,
 	st = 5,
 	sthex = 7,
@@ -23,6 +29,9 @@ enum reg {
 
 int hexChipSel = 2;
 int switchChipSel = 4;
+FILE *file;
+char *filename = "inst_mem.mif";
+int instruction_no = 0;
 
 void remove_newline(char *walker) {
 	while(walker && *walker != '\n')
@@ -43,18 +52,20 @@ char* tokenize(char *walker) {
 
 void mvi_func(int reg, int imm16) {
 	int out=0;
+	instruction_no++;
 	//mvi r5, addr
 	out = (mvi<<3) + reg;
 	out = (out<<10);
-	printf("%d\n", out);
+	fprintf(file, "\t%d   :   %d;\n", instruction_no++, out);
 
 	out = imm16;
-	printf("%d\n", out);
+	fprintf(file, "\t%d   :   %d;\n", instruction_no++, out);
 }
 
 void gen_comm(int ass, char* rx, char* ry) {
 	int regNum;
 	int out = ass;
+
 
 	sscanf(rx, "%*c%d", &regNum);
 	out = (out<<3) + regNum;
@@ -63,53 +74,92 @@ void gen_comm(int ass, char* rx, char* ry) {
 	out = (out<<3) + regNum;
 
 	out = (out<<7);	//padd with zeroes to make 16-bit
-	printf("%d\n", out);
+	fprintf(file, "\t%d   :   %d;\n", instruction_no++, out);
 }
 
-void main() {
-	char code[50] = {0};
-	fgets (code, 50, stdin);
+void initialize_file()
+{
+	fprintf(file, "WIDTH=%d;\n", WIDTH);
+	fprintf(file, "DEPTH=%d;\n\n", DEPTH);
+	fprintf(file, "ADDRESS_RADIX=UNS;\nDATA_RADIX=UNS;\n\n");
+	fprintf(file, "CONTENT BEGIN\n");
+}
 
-	char *command = code;
-	char *arg1 = tokenize(code);
-	char *arg2 = tokenize(code);
-	remove_newline(arg2);
+int main() {
 
-	int regNum;
-	int out=0;
+	file = fopen(filename, "w+");
+	initialize_file();
 
-	if(strcmp(command, "sthex")==0) { //store hex
-		//uses r5 as addr	-	0010 0000 0000 0xxx
-		//uses r6 as data	-	0000 0000 0xxx xxxx
 
-		//mvi r5, addr
-		int addr = (hexChipSel<<12) + atoi(arg1);
-		mvi_func(r5, addr);
+	while(1){
+		char code[50] = {0};
+		fgets (code, 50, stdin);
 
-		//mvi r6, data
-		mvi_func(r6, atoi(arg2));
+		if(strcmp(code, "done\n") == 0)
+			break;
 
-		//pending st
+		char *command = code;
+		char *arg1 = tokenize(code);
+		char *arg2 = tokenize(code);
+		remove_newline(arg2);
+
+		int regNum;
+		int out=0;
+
+		if(strcmp(command, "sthex")==0) { //store hex
+			//uses r5 as addr	-	0010 0000 0000 0xxx
+			//uses r6 as data	-	0000 0000 0xxx xxxx
+
+			//mvi r5, addr
+			int addr = (hexChipSel<<12) + atoi(arg1);
+			mvi_func(r5, addr);
+
+			//mvi r6, data
+			mvi_func(r6, atoi(arg2));
+
+			//st r6, r5
+			gen_comm(st, "r6", "r5");
+		}
+		else if(strcmp(command, "ldsw")==0) { //load switches
+			//uses r5 as addr	-	0100 0000 0000 0000
+			//uses r6 as target
+
+			//mvi r5, addr
+			int addr = (switchChipSel<<12);
+			mvi_func(r5, addr);
+
+			//ld r6, r5
+			out = ld;
+			out = (out<<3) + r6;
+			out = (out<<3) + r5;
+			out = (out<<7);
+			fprintf(file, "\t%d   :   %d;\n", instruction_no++, out);
+		}
+		else {
+			if(strcmp(command, "mv")==0)
+				gen_comm(mv, arg1, arg2);
+			if(strcmp(command, "mvi")==0) {
+				int reg, imm;
+				sscanf(arg1, "%*c%d", &reg);
+				imm = atoi(arg2);
+				mvi_func(reg, imm);
+			}
+			if(strcmp(command, "add")==0)
+				gen_comm(add, arg1, arg2);
+			if(strcmp(command, "sub")==0)
+				gen_comm(sub, arg1, arg2);
+			else if(strcmp(command, "ld")==0)
+				gen_comm(ld, arg1, arg2);
+			else if(strcmp(command, "st")==0)
+				gen_comm(st, arg1, arg2);
+			else if(strcmp(command, "mvnz")==0)
+				gen_comm(st, arg1, arg2);
+		}
 	}
-	else if(strcmp(command, "ldsw")==0) { //load switches
-		//uses r5 as addr	-	0100 0000 0000 0000
-		//uses r6 as target
+	
+	fprintf(file, "\t[%d..%d]  :   %d;\n", instruction_no, DEPTH-1, 0);
+	fprintf(file, "END;\n");
+	fclose(file);
 
-		//mvi r5, addr
-		int addr = (switchChipSel<<12);
-		mvi_func(r5, addr);
-
-		//ld r6, r5
-		out = ld;
-		out = (out<<3) + r6;
-		out = (out<<3) + r5;
-		out = (out<<7);
-		printf("%d\n", out);
-	}
-	else {
-		if(strcmp(command, "ld")==0)
-			gen_comm(ld, arg1, arg2);
-		else if(strcmp(command, "st")==0)
-			gen_comm(st, arg1, arg2);
-	}
+	return 0;
 }
